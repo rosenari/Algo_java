6672ee71f109a67096f9eb45d9162c406ab2524c
임장순 <rosenari88@gmail.com>
2021-10-19 22:48:04 +0900

    백준 1662 : 압축 - 정리

--- DFSBFS/Main_압축.java
+++ DFSBFS/Main_압축.java
@@ -4,32 +4,32 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
+//예외 테케 : 33(33(3)33(3))
+//output : 25
 public class Main_압축 {
-    static boolean[] v;
     public static void main(String[] args) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine().trim();
-        v = new boolean[input.toCharArray().length];
-        System.out.println(DFS(input, 0));
+        char[] chs = input.toCharArray();        
+        System.out.println(DFS(chs, 0, new boolean[chs.length]));
     }
-
-    static int DFS(String s,int index){
+    static int DFS(char[] chs, int idx, boolean[] v){
         int cnt = 0;
-        char[] str = s.toCharArray();
-        for(int i=index;i<str.length;i++){
-            if(!v[i] && str[i] == '('){
-                v[i] = true;
-                int result = DFS(s, i + 1);
-                cnt = (cnt - 1) + (result * (str[i - 1] - '0'));
-            }else if(!v[i] && str[i]==')'){
-                v[i] = true;
+
+        for(int i= idx; i < chs.length; i++){
+            if(v[i]) continue;
+            v[i] = true;
+
+            if(chs[i] == '('){
+                cnt += (DFS(chs, idx + 1, v) * (chs[i - 1] - '0'));
+                cnt--;
+            }else if(chs[i] == ')'){
                 return cnt;
-            }else if(!v[i]){
-                v[i] = true;
+            }else {
                 cnt++;
             }
         }
 
         return cnt;
-    }  
+    }
 }
